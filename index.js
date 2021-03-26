c25f09b3fe4457bcfbf36f0ca3300d3e11e0c4f6
Сергей Крюков <krus-83@mail.ru>
2021-03-26 11:34:59 +0700

    backdrop, onSelect

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import './scss/main.scss';
 
 const select = new Select('#select', {
   placeholder: 'Выберите элемент',
+  // selectedId: '2',
   data: [
     {id: '1', value: 'React'},
     {id: '2', value: 'Angular'},
@@ -11,5 +12,11 @@ const select = new Select('#select', {
     {id: '4', value: 'React Native'},
     {id: '5', value: 'Next'},
     {id: '6', value: 'Nest'}
-  ]
+  ],
+  onSelect(item) {
+    console.log('Selected item: ', item);
+  }
 });
+
+window.s = select;
+
--- select/select.js
+++ select/select.js
@@ -1,11 +1,18 @@
-const getTemplate = (placeholder, data = []) => {
-  const text = placeholder ?? '';
+const getTemplate = (placeholder, selectedId, data = []) => {
+  let text = placeholder ?? '';
 
   const itemsHtml = data.map(item => {
-    return `<li class="select__item" data-type="item" data-id="${item.id}">${item.value}</li>`
+    let classCss = '';
+    if (item.id === selectedId) {
+      text = item.value;
+      classCss = 'active'
+    }
+
+    return `<li class="select__item ${classCss}" data-type="item" data-id="${item.id}">${item.value}</li>`
   }).join('');
 
   return `
+    <div class="select__backdrop" data-type="backdrop"></div>
     <div class="select__input" data-type="input">
       <div class="select__input-text" data-type="value">
         ${text}
@@ -24,22 +31,25 @@ export class Select {
   constructor(selector, options) {
     this.$el = document.querySelector(selector);
     this.options = options;
-    this.selectedId = null;
+    this.selectedId = options.selectedId || '';
 
     this.#render();
     this.#setup();
   }
 
   #render() { // приватный метод
-    const {placeholder, data} = this.options;
+    const {placeholder, selectedId, data} = this.options;
     this.$el.classList.add('select');
-    this.$el.innerHTML = getTemplate(placeholder, data);
+    this.$el.innerHTML = getTemplate(placeholder, selectedId, data);
   }
 
   #setup() {
     this.clickHandler = this.clickHandler.bind(this);
     this.$el.addEventListener('click', this.clickHandler);
     this.$value = this.$el.querySelector('[data-type = "value"]');
+
+    // this.selectedId ? this.select(this.selectedId) : null;
+    // Сейчас как вариант элемент по умолчанию устанавливается в getTemplate()
   }
 
   clickHandler(event) {
@@ -49,7 +59,10 @@ export class Select {
       this.toggle();
     } else if (type === 'item') {
       const itemId = event.target.dataset.id
+
       this.select(itemId);
+    } else if (type === 'backdrop') {
+      this.close();
     }
   }
 
@@ -68,6 +81,13 @@ export class Select {
   select(id) {
     this.selectedId = id;
     this.$value.innerText = this.current.value;
+
+    this.$el.querySelectorAll('[data-type = "item"]').forEach(item => item.classList.remove('active'));
+
+    this.$el.querySelector(`[data-id = "${this.current.id}"]`).classList.add('active');
+
+    this.options.onSelect ? this.options.onSelect(this.current) : null;
+
     this.close();
   }
 
@@ -81,5 +101,6 @@ export class Select {
 
   destroy() {
     this.$el.removeEventListener('click', this.clickHandler);
+    this.$el.innerHTML = '';
   }
 }
